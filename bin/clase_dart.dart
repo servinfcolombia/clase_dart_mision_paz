9ebafbc10368824ad250d54f33d490e346382d34
servinfcolombia <servinfcolombia@gmail.com>
2026-03-05 19:44:00 -0500

    agregando los archivos iniciales

---
+++ bin/clase_dart.dart
@@ -0,0 +1,5 @@
+import 'package:clase_dart/clase_dart.dart' as clase_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${clase_dart.calculate()}!');
+}
---
+++ lib/clase_dart.dart
@@ -0,0 +1,11 @@
+// definicion de tipos de datos en dart
+void main(){
+int edad = 30; // tipo de dato entero
+double altura = 1.75; // tipo de dato decimal
+num peso = 70.5; // tipo de dato num (puede ser entero o decimal)
+num temperatura = 36; // tipo de dato num (puede ser entero o decimal)
+print("Mi edad es: $edad");
+print("Mi altura es: $altura");
+print("Mi peso es: $peso");
+print("Mi temperatura es: $temperatura");
+}
